da022d102ed4bbeadce95a30022332f7ef5f018a
Gurumita <g.gurumita@gmail.com>
2024-08-12 09:56:33 +0530

    ci

---
+++ src/main/java/com/example/github/github/Models/User.java
@@ -0,0 +1,50 @@
+package com.example.github.github.Models;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userid")
+    private Long id;
+
+    @Column(name = "username")
+    private String name;
+    @Column(name = "userpassword")
+    private String password;
+
+    public User() {}
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
